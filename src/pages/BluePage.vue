3d806d66b7b8802dc2eec9918616b2b32c3cf34b
Marten Erlenhein <marten.erlenhein@gmail.com>
2026-06-03 11:26:30 +0300

    Create Red, Blue and Green full-height view components

---
+++ src/pages/BluePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="hero is-link is-fullheight-with-navbar">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title is-1">Chillin... 🌊</h1>
+        <p class="subtitle is-3">This is the Blue Page</p>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/pages/RedPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="hero is-danger is-fullheight-with-navbar">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title is-1">Stop! 🛑</h1>
+        <p class="subtitle is-3">This is the Red Page</p>
+      </div>
+    </div>
+  </section>
+</template>
